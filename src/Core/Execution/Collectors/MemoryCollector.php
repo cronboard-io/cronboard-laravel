cd332bc41445bf4d7e492bce8140d5a311138466
Stefan Kovachev <stefan.kovachev@gmail.com>
2020-07-24 17:46:24 +0300

    Task-only memory usage information is now collected

--- src/Core/Execution/Collectors/MemoryCollector.php
+++ src/Core/Execution/Collectors/MemoryCollector.php
@@ -5,6 +5,8 @@ namespace Cronboard\Core\Execution\Collectors;
 class MemoryCollector extends Collector
 {
     protected $memoryUsageInBytes;
+    protected $startMemoryUsageInBytes;
+    protected $endMemoryUsageInBytes;
     protected $memoryLimitInBytes;
     protected $noLimit = false;
     protected $realUsage = false;
@@ -14,11 +16,13 @@ class MemoryCollector extends Collector
         $memoryLimitValue = ini_get('memory_limit');
         $this->noLimit = $memoryLimitValue == '-1';
         $this->memoryLimitInBytes = $this->memoryLimitToBytes($memoryLimitValue);
+        $this->startMemoryUsageInBytes = memory_get_usage($this->realUsage);
     }
 
     public function end()
     {
         $this->memoryUsageInBytes = memory_get_peak_usage($this->realUsage);
+        $this->endMemoryUsageInBytes = memory_get_usage($this->realUsage);
     }
 
     private function memoryLimitToBytes($memoryLimit): ?string
@@ -50,6 +54,9 @@ class MemoryCollector extends Collector
             'peakUsage' => $this->memoryUsageInBytes,
             'realUsage' => $this->realUsage,
             'memoryLimit' => $this->memoryLimitInBytes,
+            'taskMemoryUsage' => $this->endMemoryUsageInBytes - $this->startMemoryUsageInBytes,
+            'startUsage' => $this->startMemoryUsageInBytes,
+            'endUsage' => $this->endMemoryUsageInBytes,
             'format' => 'bytes'
         ];
     }
